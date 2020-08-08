9a4055ade0952d6aa0e16d84e3c826a13948a128
Katie Le <lethikatie@gmail.com>
2020-08-08 04:41:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,11 +15,10 @@ class Triangle
       :equilateral
     elsif (@side_1 == @side_2) || (@side_1 == @side_3) || (@side_2 == @side_3)
       :isosceles
-    
     else (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_3 != @side_1)
       :scalene
     end 
   end
   class TriangleError < StandardError
-end
+  end
 end 
